6d31f304bd5c72e7f66e59c9f4a1462a4f52f4af
C4Jay <choolakejay@gmail.com>
2020-05-19 11:23:34 +0530

    landingpage

--- screens/landingpageScreen.js
+++ screens/landingpageScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, ImageBackground, Text } from 'react-native';
+import { View, ImageBackground, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 class landingpageScreen extends Component {
     static navigationOptions = {
@@ -11,13 +11,72 @@ class landingpageScreen extends Component {
         headerTitleStyle: { 
         },
     }
+
+    newbookingHandler () {
+        console.log('new')
+    }
+
     render () {
     return (
-    <View>
-        <Text>landing page</Text>
-    </View>
+    
+        <ImageBackground style={{flex: 1}} source={{uri: 'https://images.unsplash.com/photo-1531201890865-fb64780d16e9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1575&q=80'}}>
+        {/* <View style={styles.btns}>
+        <View>
+            <View style={styles.btn}>
+                <Text>Explore all</Text>
+            </View>
+            <View style={styles.btn1}>
+                <Text>Explore by area</Text>
+            </View>
+            </View>
+        </View> */}
+
+
+<TouchableOpacity onPress={this.newbookingHandler}>
+<View style={{alignItems: 'center', marginTop: '136%'}}>
+<View style={{height: 50, width: 150, textAlign:'center', backgroundColor: 'rgba(255,255,255, 0.5)', alignItems: 'center', justifyContent:'center', borderRadius: 6}}>
+<Text style={{textAlign: 'center'}}>Explore all</Text>
+</View>
+        </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this.newbookingHandler}>
+<View style={{alignItems: 'center', marginTop: 10}}>
+<View style={{height: 50, width: 150, textAlign:'center', backgroundColor: 'rgba(255,255,255, 0.5)', alignItems: 'center', justifyContent:'center', borderRadius: 6}}>
+<Text style={{textAlign: 'center'}}>Explore by region</Text>
+</View>
+        </View>
+        </TouchableOpacity>
+    
+        </ImageBackground>
+    
     )
     }
 }
+
+
+const styles = StyleSheet.create({
+    btns: {
+        zIndex: 1,
+        marginTop: '50%',
+        alignItems: 'center',
+       
+    },
+    btn1: {
+        marginTop: 30,
+        borderStyle: 'solid',
+        borderColor: 'black',
+        borderWidth: 1,
+        borderBottomWidth: 1,
+        backgroundColor: 'white'
+    },
+    btn: {
+        marginTop: 30,
+        borderStyle: 'solid',
+        borderColor: 'black',
+        borderWidth: 1,
+        borderBottomWidth: 1,
+        backgroundColor: 'white'
+    }
+})
 
 export default landingpageScreen;
